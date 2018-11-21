9464dc093a474d0eeb55c58695609b4e4f1c2672
michbur <michalburdukiewicz@gmail.com>
2018-11-21 18:31:03 +0100

    aligned

--- rotating.R
+++ rotating.R
@@ -7,9 +7,10 @@ image_read_svg("Flag_of_Denmark.svg") %>%
   image_write("Flag_of_Denmark_r.svg")
 
 
-data.frame(x = 1L:2,
-           y = 1L:2,
-           img = c("Flag_of_Denmark.svg",
-                   "Flag_of_Denmark_r.svg")) %>% 
-  ggplot(aes(x = x, y = y, image = img)) + 
-  geom_image() 
+data.frame(x = 1L:10,
+           y = 1L:10,
+           img = sample(c("Flag_of_Denmark.svg",
+                   "Flag_of_Denmark_r.svg"), 10, replace = TRUE)) %>% 
+  ggplot(aes(x = x, y = y, image = img)) +
+  geom_abline(slope = 1, intercept = 0) +
+  geom_image(size = 0.05) 
